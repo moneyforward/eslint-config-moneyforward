91bcb4c1ea1e74a66b335447225cd45f184dbd97
YAMADA Naoki <wakamsha@gmail.com>
2024-03-06 11:50:21 +0900

    feat: Implement v3 rule "node" (#215)
    * replace eslint-plugin-node to eslint-plugin-n
    
    * feat: Implement the rule "node"

--- rules/node.js
+++ rules/node.js
@@ -1,4 +1,17 @@
 module.exports = {
-  extends: ['../configs/node'].map(require.resolve),
-  rules: {},
+  plugins: ['n'],
+  extends: ['plugin:n/recommended'],
+  rules: {
+    // require all requires be top-level
+    // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/global-require.md
+    'n/global-require': ['error'],
+
+    // disallow use of new operator with the require function
+    // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/no-new-require.md
+    'n/no-new-require': ['error'],
+
+    // disallow string concatenation with __dirname and __filename
+    // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/no-path-concat.md
+    'n/no-path-concat': ['error'],
+  },
 };
