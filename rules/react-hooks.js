c673a50f14e97cfe2735bd0d1dae8211b25636c3
wakamsha <wakamsha@gmail.com>
2024-03-11 14:45:16 +0900

    add comments

--- rules/react-hooks.js
+++ rules/react-hooks.js
@@ -2,7 +2,12 @@ module.exports = {
   plugins: ['react-hooks'],
 
   rules: {
+    // Verify the list of the dependencies for Hooks like useEffect and similar.
+    // https://github.com/facebook/react/blob/main/packages/eslint-plugin-react-hooks/src/ExhaustiveDeps.js
     'react-hooks/exhaustive-deps': ['error'],
+
+    // Enforce Rules of Hooks.
+    // https://github.com/facebook/react/blob/main/packages/eslint-plugin-react-hooks/src/RulesOfHooks.js
     'react-hooks/rules-of-hooks': ['error'],
   },
 };
