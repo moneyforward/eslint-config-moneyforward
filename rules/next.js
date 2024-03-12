5791d29c4ae19f2e59f2d9c6ca96e4f804552c78
wakamsha <wakamsha@gmail.com>
2024-03-12 16:03:54 +0900

    fix
    fix

--- rules/next.js
+++ rules/next.js
@@ -1,7 +1,8 @@
 module.exports = {
   plugins: ['@next/next'],
   extends: [
-    'plugin:@next/next/recommended',
+    // This config includes `plugin:@next/next/recommended`
+    // https://github.com/vercel/next.js/blob/v14.1.3/packages/eslint-plugin-next/src/index.ts#L56
     'plugin:@next/next/core-web-vitals',
   ],
   rules: {},
