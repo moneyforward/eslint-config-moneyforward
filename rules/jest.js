d544fa0e6e99cd27cdad6110bec57c400d29c1c9
YAMADA Naoki <wakamsha@gmail.com>
2024-03-12 17:51:18 +0900

    feat: Implement v3 rule "jest" (#228)
    * feat: Implement the rule "jest"
    
    * update eslint-plugin-jest

--- rules/jest.js
+++ rules/jest.js
@@ -1,4 +1,9 @@
 module.exports = {
-  extends: ['../configs/jest'].map(require.resolve),
-  rules: {},
+  plugins: ['jest'],
+  extends: ['plugin:jest/recommended', 'plugin:jest/style'],
+  rules: {
+    // Disallow duplicate setup and teardown hooks
+    // https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/no-duplicate-hooks.md
+    'jest/no-duplicate-hooks': ['error'],
+  },
 };
