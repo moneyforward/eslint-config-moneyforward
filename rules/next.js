b1662ad1fcaf1f18eb6cc70785e32c61986389a5
YAMADA Naoki <wakamsha@gmail.com>
2024-03-12 17:44:23 +0900

    feat: Implement v3 rule "next" (#227)
    * install `@next/eslint-plugin-next`
    
    * implement rule "next"
    
    * fix
    
    fix

--- rules/next.js
+++ rules/next.js
@@ -1,4 +1,9 @@
 module.exports = {
-  extends: ['../configs/next'].map(require.resolve),
+  plugins: ['@next/next'],
+  extends: [
+    // This config includes `plugin:@next/next/recommended`
+    // https://github.com/vercel/next.js/blob/v14.1.3/packages/eslint-plugin-next/src/index.ts#L56
+    'plugin:@next/next/core-web-vitals',
+  ],
   rules: {},
 };
