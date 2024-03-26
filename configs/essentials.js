af7c7757ff8185942ac0ca5f606bafcd365414b4
YAMADA Naoki <wakamsha@gmail.com>
2024-03-26 15:35:34 +0900

    feat: Implement v3 config "essentials" (#237)
    * specify settings in rules/imports
    
    * implement config `essentials`
    
    fix

---
+++ configs/essentials.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    '../rules/best-practices',
+    '../rules/errors',
+    '../rules/es6',
+    '../rules/imports',
+    '../rules/promise',
+    '../rules/style',
+    '../rules/variables',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+};
