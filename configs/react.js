4916e23d9dd2bd3be447650d704d94e379fc4d55
wakamsha <wakamsha@gmail.com>
2024-03-28 14:23:02 +0900

    feat: Implement config "react"

--- configs/react.js
+++ configs/react.js
@@ -1,25 +1,7 @@
 module.exports = {
-  extends: [
-    // https://github.com/yannickcr/eslint-plugin-react/blob/9f1d618499b23e184a628365cb5e0ea48292cd6f/index.js
-    'plugin:react/recommended',
-    // https://github.com/facebook/react/blob/73ffce1b6fb7eff13414e35e40efe4269664e5ea/packages/eslint-plugin-react-hooks/src/index.js
-    'plugin:react-hooks/recommended',
-  ],
-  plugins: ['react'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  rules: {
-    // don't have to use props-types
-    'react/prop-types': 'off',
-    // don't have to import React
-    'react/react-in-jsx-scope': 'off',
-  },
+  extends: ['../rules/react', '../rules/react-hooks', '../rules/jsx-a11y'],
   settings: {
     react: {
-      // https://github.com/yannickcr/eslint-plugin-react/issues/1955
       version: 'detect',
     },
   },
