981473b1da3a4947a05aa42cc534a91beff93766
wakamsha <wakamsha@gmail.com>
2024-03-29 13:11:13 +0900

    fix config

--- configs/storybook.js
+++ configs/storybook.js
@@ -2,7 +2,11 @@ module.exports = {
   overrides: [
     {
       extends: ['../rules/storybook'],
-      files: ['*.stories.@(ts|tsx|js|jsx)', '*.story.@(ts|tsx|js|jsx)'],
+      files: [
+        '*.stories.@(ts|tsx|js|jsx)',
+        '*.story.@(ts|tsx|js|jsx)',
+        '**/.storybook/**/*.@(ts|tsx|js|jsx)',
+      ],
     },
   ],
 };
