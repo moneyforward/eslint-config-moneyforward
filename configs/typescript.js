e1b8377dd2e7ef44172270b92363cf6544fe8d2d
wakamsha <wakamsha@gmail.com>
2024-03-29 13:58:07 +0900

    implement config `typescript`

--- configs/typescript.js
+++ configs/typescript.js
@@ -1,16 +1,9 @@
 module.exports = {
   overrides: [
     {
-      files: ['**/*.ts?(x)'],
-      extends: [
-        // https://github.com/typescript-eslint/typescript-eslint/blob/8cfe93372e1d826e54febc3aeb7047c792b90963/packages/eslint-plugin/src/configs/recommended.ts
-        'plugin:@typescript-eslint/recommended',
-      ],
-      plugins: ['@typescript-eslint'],
+      extends: ['../rules/typescript'],
+      files: ['*.@(ts|tsx|cts|mts)'],
       parser: '@typescript-eslint/parser',
-      parserOptions: {
-        sourceType: 'module',
-      },
     },
   ],
 };
