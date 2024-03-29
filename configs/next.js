5e663301e62bb076c6ed13852bf5f074e7ed300d
wakamsha <wakamsha@gmail.com>
2024-03-29 18:31:25 +0900

    implement config `next`

--- configs/next.js
+++ configs/next.js
@@ -1,8 +1,3 @@
 module.exports = {
-  extends: [
-    require.resolve('./react'),
-    // https://github.com/vercel/next.js/blob/6f420962338fb99a377bde8e1e663bc202c95b68/packages/eslint-plugin-next/lib/index.js
-    'plugin:@next/next/recommended',
-  ],
-  plugins: ['@next/next'],
+  extends: ['../rules/next'],
 };
