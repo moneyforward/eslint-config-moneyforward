fed9f7ae09be8a54a4214fb8195e6d6f37bb7afb
wakamsha <wakamsha@gmail.com>
2024-04-02 15:15:29 +0900

    remove unnecessary configs

--- configs/base.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  env: {
-    browser: true,
-  },
-  extends: [
-    // https://github.com/eslint/eslint/blob/f113cdd872257d72bbd66d95e4eaf13623323b24/conf/eslint-recommended.js
-    'eslint:recommended',
-  ],
-};
