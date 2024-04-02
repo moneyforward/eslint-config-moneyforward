0486fb5cdab4b8bab0ebcf6c607c9b36c479c77a
wakamsha <wakamsha@gmail.com>
2024-04-02 15:07:49 +0900

    feat: Implement config "node"

--- configs/node.js
+++ configs/node.js
@@ -1,9 +1,6 @@
 module.exports = {
+  extends: ['../rules/node'],
   env: {
     node: true,
   },
-  extends: [
-    // https://github.com/mysticatea/eslint-plugin-node/blob/f45c6149be7235c0f7422d1179c25726afeecd83/lib/index.js
-    'plugin:node/recommended',
-  ],
 };
