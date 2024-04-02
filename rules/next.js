231de8259ce08a9a5d5d72df60c91cf3f112154b
wakamsha <wakamsha@gmail.com>
2024-04-02 10:59:20 +0900

    add required file names
    fix glob pattern

--- rules/next.js
+++ rules/next.js
@@ -11,7 +11,9 @@ module.exports = {
       files: [
         '**/pages/**/*.@(tsx|jsx|js)',
         '*.page.@(tsx|jsx|js)',
-        '**/app/**/@(page|layout).@(tsx|jsx|js)',
+        // Files with these names allow `default export` according to the Next.js specifications.
+        // https://nextjs.org/docs/app/api-reference/file-conventions
+        '**/app/**/@(default|error|layout|loading|middleware|not\\-found|page|template).*',
       ],
       rules: {
         'import/no-default-export': ['off'],
