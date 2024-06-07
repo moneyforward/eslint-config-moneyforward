ed436fe45c3cbe86cfd0d4c02866fe6514196098
wakamsha <wakamsha@gmail.com>
2024-06-07 16:56:21 +0900

    chore: Upate snapshot test log

--- tests/snapshot-test/node/snapshot.test.js
+++ tests/snapshot-test/node/snapshot.test.js
@@ -1,7 +1,7 @@
 const { getESLintConfig, optimizeRules } = require('../helper');
 
 test('should match ESLint configuration snapshot: node', async () => {
-  const filePath = 'dummy.tsx';
+  const filePath = './dummy.js';
   const { rules, settings } = await getESLintConfig(filePath, __dirname);
 
   expect({ rules: optimizeRules(rules), settings }).toMatchSnapshot();
