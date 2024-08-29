0c16c1d51aa41188769b6234478e5bb4bdd2e352
YAMADA Naoki <wakamsha@gmail.com>
2024-08-29 11:49:59 +0900

    ci: Recognize `!` symbol as `BREAKING CHANGE:` (#318)
    * install package
    
    * use preset
    
    fix

--- release.config.js
+++ release.config.js
@@ -7,13 +7,19 @@ module.exports = {
     [
       '@semantic-release/commit-analyzer',
       {
+        preset: 'conventionalcommits',
         releaseRules: [
           { type: 'docs', scope: 'README', release: 'patch' },
           { type: 'docs', scope: 'LICENSE', release: 'patch' },
         ],
       },
     ],
-    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+      },
+    ],
     '@semantic-release/changelog',
     '@semantic-release/npm',
     [
