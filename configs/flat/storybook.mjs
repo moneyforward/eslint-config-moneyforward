21713948ef6dbf446ef5d6501b26314ab7a6085e
YAMADA Naoki <wakamsha@gmail.com>
2024-12-07 13:19:58 +0900

    feat: Update eslint-plugin-storybook version (#352)
    * feat: Update eslint-plugin-storybook version
    
    * update snapshot
    
    * update snapshot
    
    * update snapshot
    
    * update snapshot

--- configs/flat/storybook.mjs
+++ configs/flat/storybook.mjs
@@ -1,42 +1,23 @@
 // @ts-check
 // @ts-ignore
 import importPlugin from 'eslint-plugin-import';
-import * as storybook from 'eslint-plugin-storybook';
-import storybookRuleSetBase from '../../rules/storybook.js';
-
-const storybookRuleSetStories = {
-  plugins: {
-    storybook,
-    import: importPlugin,
-  },
-
-  rules: {
-    ...storybook.configs.recommended.overrides[0].rules,
-    ...storybook.configs['csf-strict'].rules,
-    ...storybookRuleSetBase.rules,
-    'import/no-default-export': ['off'],
-  },
-};
-
-const storybookRuleSetConfig = {
-  plugins: {
-    storybook,
-    import: importPlugin,
-  },
-  rules: {
-    ...storybook.configs.recommended.overrides[1].rules,
-    'import/no-default-export': ['off'],
-  },
-};
+import storybook from 'eslint-plugin-storybook';
 
 export default [
   {
-    files: ['**/*.@(stories|story).@(ts|tsx|js|jsx|mjs|cjs)'],
-    ...storybookRuleSetStories,
+    plugins: {
+      storybook,
+      import: importPlugin,
+    },
   },
 
+  ...storybook.configs['flat/recommended'],
+  ...storybook.configs['flat/csf-strict'],
+
   {
-    files: ['**/.storybook/**/*.@(ts|tsx|js|jsx|mjs|cjs)'],
-    ...storybookRuleSetConfig,
+    files: ['**/*.@(stories|story).@(ts|tsx|js|jsx|mjs|cjs)'],
+    rules: {
+      'import/no-default-export': ['off'],
+    },
   },
 ];
